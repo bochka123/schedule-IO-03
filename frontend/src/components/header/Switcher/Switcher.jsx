2e843155b450c6146ab4e616afa24741692dbb7f
double-hope <nadia.prohorchuk@gmail.com>
2022-09-12 01:13:01 +0300

    add Icons to page

--- frontend/src/components/header/Switcher/Switcher.jsx
+++ frontend/src/components/header/Switcher/Switcher.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import "./Switcher.css"
+import {Icon} from "../../primitives/icon/component";
+import {Icons} from "../../../common/enums";
 const Switcher = () => {
     return (
-        <div>
-            <i className="fa-regular fa-sun-bright"></i>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <Icon icon={Icons.SUN} color='blue'/>
             <div className="switcher-out">
                 <div className="switcher-in">
 
                 </div>
             </div>
-            <i className="fa-light fa-moon"></i>
+                <Icon icon={Icons.MOON} color='yellow' />
         </div>
     );
 };
