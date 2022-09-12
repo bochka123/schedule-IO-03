a6978d268feaa69289850d0031c1ca0d7a1d1aed
bochka123 <sasha2003tkach001100@gmail.com>
2022-09-13 00:38:35 +0300

    main grid

---
+++ frontend/src/components/main/Main.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./Main.css"
+
+const Main = () => {
+    return (
+        <main>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </main>
+    );
+};
+
+export default Main;
