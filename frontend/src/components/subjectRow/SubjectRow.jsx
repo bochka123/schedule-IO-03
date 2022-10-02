91deed49397ca3fd9b788fcf6e1d25f7273b2a3d
bochka123 <sasha2003tkach001100@gmail.com>
2022-10-03 00:24:25 +0300

    add subject rows

---
+++ frontend/src/components/subjectRow/SubjectRow.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./SubjectRow.css"
+
+const SubjectRow = ({values}) => {
+    return (
+        <section>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
+    );
+};
+
+export default SubjectRow;
