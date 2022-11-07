3d0a44a512255149d1d0d096ded06a1e6ba6a582
double-hope <nadia.prohorchuk@gmail.com>
2022-11-07 22:58:18 +0200

    create server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello world!")
+import express from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
