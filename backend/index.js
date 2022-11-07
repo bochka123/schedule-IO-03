da67fb7f2b55c00167242f196511ac8ede17ede0
double-hope <nadia.prohorchuk@gmail.com>
2022-11-07 23:13:57 +0200

    start backend with nodemon

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,20 @@ import LessonModel from './models/Lesson.js';
 const app = express();
 const PORT = 3001;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    // const doc = new LessonModel({
+    //     id: "1",
+    //     name: "Паралельне програмування",
+    //     lecturer: "Корочкін Олександр Володимирович",
+    //     deadline: "2019-12-31",
+    //     task: "(-_-)",
+    //     notes: "^_^",
+    //     link: "https://google.com"
+    // })
+
+    // const lesson = await doc.save();
+
     res.send('Hello');
-    const doc = new LessonModel({
-        id: "1",
-        name: "Паралельне програмування",
-        lecturer: "Корочкін Олександр Володимирович",
-        deadline: "2019-12-31",
-        task: "(-_-)",
-        notes: "^_^",
-        link: "https://google.com"
-    })
 })
 
 app.listen(PORT, () => {
