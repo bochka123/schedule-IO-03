db72abcd0eb43cbd1cf1ec1dd47448a4c0dfb143
double-hope <nadia.prohorchuk@gmail.com>
2022-11-08 03:24:26 +0200

    use redux toolkit

--- frontend/src/store/subjects/actions.js
+++ frontend/src/store/subjects/actions.js
@@ -1,32 +1,8 @@
 import { ActionType } from './common';
-import { DataStatus } from "../../common/enums";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const setStatus = (status) =>({
-    type: ActionType.SET_STATUS,
-    payload: {
-        status,
-    }
-});
-
-const fetchSubjects = () => async (dispatch, getStore, { subjectsService }) => {
-
-    dispatch(setStatus(DataStatus.PENDING));
-
-    try{
-        const subjects = await subjectsService.getAll();
-
-        dispatch({
-            type: ActionType.SET_SUBJECTS,
-            payload:{
-                subjects,
-            },
-        });
-
-        dispatch(setStatus(DataStatus.SUCCESS));
-    } catch {
-        dispatch(setStatus(DataStatus.ERROR));
-    }
-
-}
+const fetchSubjects = createAsyncThunk(ActionType.GET, async (_args, { extra }) => ({
+    subjects: await extra.subjectsService.getAll(),
+}));
 
 export { fetchSubjects };
